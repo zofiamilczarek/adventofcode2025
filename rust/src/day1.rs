5b137b97f5f4fab8abd41dccfc95d66f70f8a2d6
zofiamilczarek <zmilcz@gmail.com>
2025-12-02 12:11:10 +0100

    got the 2nd star

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -32,7 +32,7 @@ fn get_rotation_value(rotation_code: &String) -> i32  {
 }
 
 
-fn get_secret_code(rotations: Vec<String> ) -> i32{
+fn get_secret_code(rotations: &Vec<String> ) -> i32{
     let mut nb_zeros = 0;
     let mut current_tick = 50;
     let mut value: i32;
@@ -48,20 +48,86 @@ fn get_secret_code(rotations: Vec<String> ) -> i32{
         current_tick = new_tick;
     };
 
-    return nb_zeros;
+   nb_zeros
+}
+
+
+fn get_secret_code_part2_iter(rotations: &Vec<String>) -> i32 {
+    let mut value;
+    let mut sign;
+    let mut nb_zeros = 0;
+    let mut current_tick = 50;
+    for rotation in rotations.iter() {
+        value = get_rotation_value(rotation);
+        sign = value.signum();
+        value = value.abs();
+        // println!("Rotation : {} {}", sign, value);
+        for tick in 0..value {
+            current_tick = (current_tick + sign).rem_euclid(100);
+            if current_tick == 0 {
+                nb_zeros+=1;
+            }
+            // println!("  Tick : {}", current_tick);
+        }
+    };
+    nb_zeros
+}
+
+
+fn get_secret_code_part2(rotations: &Vec<String>) -> i32 {
+    let mut total_nb_zeros = 0;
+    let mut current_tick = 50;
+    let mut new_tick: i32;
+    let mut value: i32;
+    let mut nb_zeros = 0;
+
+    for rotation in rotations.iter() {
+        value = get_rotation_value(rotation);
+        new_tick = (current_tick + value).rem_euclid(100);
+        
+        if value >= 0 {
+            nb_zeros = (current_tick + value) / 100;
+        }
+        else {
+            if current_tick == 0 {
+                nb_zeros = value.abs() / 100;
+            }
+            else if value.abs() > current_tick {
+                nb_zeros = ((value.abs() - current_tick - 1) / 100) + 1;
+                if new_tick == 0 {
+                    nb_zeros += 1;
+                }
+            else if value.abs() == current_tick {
+                nb_zeros = 1
+            }
+            }
+        }
+        println!("start tick : {}, end tick : {}, rotation : {}, nb passses through 0: {}", current_tick, new_tick, rotation, nb_zeros);
+        total_nb_zeros += nb_zeros;
+        current_tick = new_tick;
+
+    };
+
+    total_nb_zeros
 }
 
 
 pub fn print_answer() {
-    // let input : Vec<String> = vec!["L68", "L30", "R48","L5","R60","L55","L1","L99","R14","L82"]
-    //                         .iter()
-    //                         .map(|x| x.to_string())
-    //                         .collect();
+    let test_input : Vec<String> = vec!["L68", "L30", "R48","L5","R60","L55","L1","L99","R14","L82"]
+                            .iter()
+                            .map(|x| x.to_string())
+                            .collect();
 
-    let input = lines_from_file("/Users/zofia/Documents/projects/adventofcode2025/input_day1.txt");
+    let real_input = lines_from_file("/Users/zofia/Documents/projects/adventofcode2025/input_day1.txt");
 
-    let result = get_secret_code(input);
+    let input = real_input;
 
-    println!("The secret code : {}", result);
+    let result_1 = get_secret_code(&input);
+
+    let result_2 = get_secret_code_part2_iter(&input);
+
+    println!("The (simple) secret code : {}", result_1);
+
+    println!("The (harder) secret code : {}", result_2);
 
 }

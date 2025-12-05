bb029b252b5216c3e2b6632c1b3ea1e5a97b8da7
zofiamilczarek <zmilcz@gmail.com>
2025-12-05 17:55:54 +0100

    day 5 not working

---
+++ src/day5.rs
@@ -0,0 +1,59 @@
+#[path = "utils.rs"]
+mod utils;
+use core::num;
+
+use crate::day5::utils::utils::lines_from_file;
+
+
+
+fn is_fresh(number: i64, ranges : &Vec<[i64;2]>) -> bool{
+    
+    for [a, b] in &ranges[1..]{
+        if (a..=b).contains(&&number) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn get_numbers_ranges(lines : &Vec<String>) -> (Vec<i64>, Vec<[i64;2]>){
+    let mut numbers = Vec::new();
+    let mut ranges: Vec<[i64;2]> = Vec::new();
+    let mut curr_range;
+    let mut is_number =true;
+
+    for line in lines{
+        println!("{line}");
+        if line == &"".to_string() {
+            is_number = false;
+        }
+        else {
+            if is_number {
+            numbers.push(line.parse::<i64>().unwrap());
+        }
+        else {
+            // curr_range = line.split('-').map(|x| x.parse::<u64>());
+
+            ranges.push(line.split('-')
+                                        .map(|x| x.parse::<i64>().unwrap())
+                                        .collect::<Vec<i64>>().try_into().unwrap());
+        }
+        }
+
+        
+    }
+
+    return (numbers, ranges);
+}
+
+
+pub fn print_answer() {
+
+    let lines = lines_from_file("input_day5.txt");
+    let numbers;
+    let ranges;
+    (numbers, ranges) = get_numbers_ranges(&lines);
+    
+    println!("{:#?}\n\n{:#?}", numbers, ranges);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
-    println!("\n=== Answers for Day 1 ===");
-    day1::print_answer();
+    // println!("\n=== Answers for Day 1 ===");
+    // day1::print_answer();
 
-    println!("\n=== Answers for Day 2 ===");
-    day2::print_answer();
+    // println!("\n=== Answers for Day 2 ===");
+    // day2::print_answer();
 
-    println!("\n=== Answers for Day 3 ===");
-    day3::print_answer();
+    // println!("\n=== Answers for Day 3 ===");
+    // day3::print_answer();
 
-    println!("\n=== Answers for Day 4 ===");
-    day4::print_answer();
+    // println!("\n=== Answers for Day 4 ===");
+    // day4::print_answer();
+
+    println!("\n=== Answers for Day 5 ===");
+    day5::print_answer();
 }
